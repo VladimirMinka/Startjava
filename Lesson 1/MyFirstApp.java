366aa76f603578033e8e2c25a2a634c43f34c460
Vladimir.Minka <vladaha@gmail.com>
2019-03-22 23:21:38 +0300

    My First App

--- Lesson 1/MyFirstApp.java
+++ Lesson 1/MyFirstApp.java
@@ -1,5 +1,5 @@
-public class MyFirstApp {
- public static void main (String[] arga) {
- System.out.println("Hello, World!");
- }
- }
+public class MyFirstApp {	
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
