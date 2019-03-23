88ffc13f956b55ecfdcf314cf88d167729cf4f99
Vladimir.Minka <vladaha@gmail.com>
2019-03-24 00:23:38 +0300

    Home Work Variable

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,24 +1,24 @@
-    public class Variable {
+public class Variable {
     public static void main(String[] args) {
-    byte  ram = 8;  
-    short rom = 512;
-    int corei = 5;
-    long allmemory = 1000;
-    float cpufrequency = 1.8f;
-    double display  =  15.6;
-    char usbtype = 'c';
-    boolean laptop = true;
+        byte  ram = 8;  
+        short rom = 512;
+        int corei = 5;
+        long allmemory = 1000;
+        float cpufrequency = 1.8f;
+        double display  =  15.6;
+        char usbtype = 'c';
+        boolean laptop = true;
         {
 
-    System.out.println(ram);
-    System.out.println(rom);
-    System.out.println(corei);
-    System.out.println(allmemory);
-    System.out.println(cpufrequency);   
-    System.out.println(display);
-    System.out.println(usbtype);
-    System.out.println(laptop);
+        System.out.println(ram);
+        System.out.println(rom);
+        System.out.println(corei);
+        System.out.println(allmemory);
+        System.out.println(cpufrequency);   
+        System.out.println(display);
+        System.out.println(usbtype);
+        System.out.println(laptop);
     }
-    }
-    }      
+}
+}   
       
