8abddbcba88b64d0a3f93127a5d24deb82081451
Vladimir.Minka <vladaha@gmail.com>
2019-03-28 22:42:06 +0300

    Внес изменения в ДЗ

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,21 +1,21 @@
 public class Variable {
-	public static void main(String[] args) {        
-		byte ram = 8;  
-		short rom = 512;
-		int corei = 5;
-		long allMemory = 1000;
-		float cpuFrequency = 1.8f;
-		double display = 15.6;
-		char usbType = 'c';
-		boolean laptop = true;
-		
-		System.out.println(ram);
-		System.out.println(rom);
-		System.out.println(corei);
-		System.out.println(allMemory);
-		System.out.println(cpuFrequency);   
-		System.out.println(display);
-		System.out.println(usbType);
-		System.out.println(laptop);
-	}
+    public static void main(String[] args) {        
+        byte ram = 8;  
+        short rom = 512;
+        int corei = 5;
+        long allMemory = 1000;
+        float cpuFrequency = 1.8f;
+        double display = 15.6;
+        char usbType = 'c';
+        boolean laptop = true;
+        
+        System.out.println(ram);
+        System.out.println(rom);
+        System.out.println(corei);
+        System.out.println(allMemory);
+        System.out.println(cpuFrequency);   
+        System.out.println(display);
+        System.out.println(usbType);
+        System.out.println(laptop);
+    }
 }  
