e3fc7b6888b33e871bdacce47dc8116cab1d0f83
Vladimir.Minka <vladaha@gmail.com>
2019-03-28 22:13:57 +0300

    Внес изменения в ДЗ

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,34 +1,34 @@
 public class ConditionalStatement {
-    public static void main(String[] args) {
-        int age = 37;
-        double growth = 1.83;
-        boolean male = true;
-        char name = 'В';
+	public static void main(String[] args) {
+		int age = 37;
+		double growth = 1.83;
+		boolean male = true;
+		char name = 'В';
 
-        if (age > 20) {
-            System.out.println("Мне больше 20 лет" + "," +  " "+ "Мне" + " " + age + " " + " лет");
-        }
+		if (age > 20) {
+			System.out.println("Мне больше 20 лет" + "," +  " " + "Мне" + " " + age + " " + " лет");
+		}
 
-        if (male) {
-            System.out.println("Я мужчина");
-        }
+		if (male) {
+			System.out.println("Я мужчина");
+		}
 
-        if (!male) {
-            System.out.println("Я женщина");
-        }
+		if (!male) {
+			System.out.println("Я женщина");
+		}
 
-        if (growth < 1.80) {
-            System.out.println("Мой рост 1.80 м");
-        } else {
-            System.out.println("Мой рост больше 1.80" + ", " + "мой рост" + " " + growth);
-        }
+		if (growth < 1.80) {
+			System.out.println("Мой рост 1.80 м");
+		} else {
+			System.out.println("Мой рост больше 1.80" + ", " + "мой рост" + " " + growth);
+		}
 		
-        if (name == 'В') {
-            System.out.println("Моё имя начинается с буквы:"+ " " + name);
+		if (name == 'В') {
+			System.out.println("Моё имя начинается с буквы:" + " " + name);
 	} else if (name == 'И') {
-            System.out.println("Моё имя начинается  с буквы:" + name);
-        } else {
-            System.out.println("Моё имя начинается с неизвестной буквы:" + name);
+			System.out.println("Моё имя начинается  с буквы:" + name);
+		} else {
+			System.out.println("Моё имя начинается с неизвестной буквы:" + name);
 	}
-    }
+	}
 }
