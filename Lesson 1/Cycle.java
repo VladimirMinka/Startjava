65ae133cd4bc232561cf98aa8c12a4944bc97015
Vladimir.Minka <vladaha@gmail.com>
2019-03-29 23:21:14 +0300

    Выполнил ДЗ

---
+++ Lesson 1/Cycle.java
@@ -0,0 +1,21 @@
+public class Cycle {
+	public static void main(String[] args) {
+		for(int i = 0; i<20; i++ ) {
+		System.out.println(i);
+	}
+		int a = 6;
+		while(a >=-6) {
+		System.out.println(a);
+		a-=2;
+	}
+	{
+		int b = 10;
+		do()
+		b+=2;
+		while(b < 20);
+			
+		
+	}
+}
+}
+}
