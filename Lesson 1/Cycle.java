446e499975888d0c4ac4235e199de60b58a4cd50
Vladimir.Minka <vladaha@gmail.com>
2019-03-30 23:20:27 +0300

    Исправил в соответствии с замечаниями

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,13 +1,13 @@
 public class Cycle {
     public static void main(String[] args) {
         for(int i = 0; i <= 20; i++ ) {
-        System.out.println(i);
+            System.out.println(i);
         }
 
         int a = 6;
         while(a >=- 6) {
-        System.out.println(a);
-        a -= 2;
+            System.out.println(a);
+            a -= 2;
         }
 
         int numbers = 10;
@@ -16,7 +16,7 @@ public class Cycle {
             if (numbers % 2 == 1) {
                 sum += numbers;
             }
-        numbers++;
+            numbers++;
         } while (numbers < 20);
         System.out.print(sum);
     }
