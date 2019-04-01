00047b8acee2c774dc55d39485402a454b019b97
Vladimir.Minka <vladaha@gmail.com>
2019-04-01 22:27:28 +0300

    Добавил решение по упражнению Калькулятор

---
+++ Lesson 1/Calculate.java
@@ -0,0 +1,27 @@
+public class Calculate  {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 60;
+        char operation = '*';
+        int result = 0;
+        if (a > 0 && b > 0) {
+            if (operation == '+') {
+                result = a + b;
+                } else if (operation == '-') {
+                    result = a - b;
+                } else if (operation == '*') {
+                    result = a * b;
+                } else if (operation == '/') {
+                    result = a / b;
+                } else if (operation == '^') {
+                    result = a ^ b;
+                } else if (operation == '%') {
+                    result = a % b;
+                }  System.out.println("Result is equal:" + " " + result);
+                }  else {
+                    System.out.println("The calculator works only with positive integers");
+                }
+
+        } 
+}
+
