3f79139a73e1abd2cf63d5436102bdf7bc563ec4
Vladimir.Minka <vladaha@gmail.com>
2019-04-01 22:40:29 +0300

    Добавил решение по задаче Угадай число

---
+++ Lesson 1/Game.java
@@ -0,0 +1,11 @@
+public class Game  {
+    public static void main(String[] args) {
+        int inputnumber = 3; 
+        int answer = 50;
+        System.out.println("Guess number from 0 to 100.");
+        System.out.println("The number entered by you is more (less) than the fact that thought of the computer");
+        if(inputnumber == answer)
+        System.out.println("You guessed");
+        else System.out.println("You did not guessed");
+   }
+}
