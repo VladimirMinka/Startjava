7aea25de9121668e4113e7aca7878ebea4ac18a3
Vladimir.Minka <vladaha@gmail.com>
2019-04-04 22:56:32 +0300

    ИСправил замечания по задаче Calculate

--- Lesson 1/Calculate.java
+++ Lesson 1/Calculate.java
@@ -1,27 +1,32 @@
-public class Calculate  {
+public class Calculate {
     public static void main(String[] args) {
         int a = 50;
         int b = 60;
         char operation = '*';
         int result = 0;
         if (a > 0 && b > 0) {
-            if (operation == '+') {
+        if (operation == '+') {
                 result = a + b;
-                } else if (operation == '-') {
+        } else if (operation == '-') {
                     result = a - b;
-                } else if (operation == '*') {
+        } else if (operation == '*') {
                     result = a * b;
-                } else if (operation == '/') {
+        } else if (operation == '/') {
                     result = a / b;
-                } else if (operation == '^') {
-                    result = a ^ b;
-                } else if (operation == '%') {
+        } else if (operation == '^') {
+                    result = a;
+                    int i = b;
+                    while (i > 1) {
+                    result *=a;
+                    i--;
+                    }
+        } else if (operation == '%') {
                     result = a % b;
-                }  System.out.println("Result is equal:" + " " + result);
-                }  else {
-                    System.out.println("The calculator works only with positive integers");
                 }
-
-        } 
+        System.out.println("Result is equal:" + " " + result);
+        }  else {
+        System.out.println("The calculator works only with positive integers");
+        }
+        }
 }
 
