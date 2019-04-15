42bf03557bdc9f250587ac9d3ec8d0c4365255f4
Vladimir.Minka <vladaha@gmail.com>
2019-04-15 22:48:24 +0300

    Выполнил первое ДЗ Второго урока

---
+++ Lesson 2/Person.java
@@ -0,0 +1,35 @@
+public class Person {
+
+    public static void main(String[] args) {
+    //пол, имя, рост, вес, возраст
+
+    double height = 1.83;
+    double weight = 93.0;
+    int age = 37;
+    String sex = "man";
+    String name = "Vladimir";
+    // идти, сидеть, бежать, говорить, учить Java
+
+    void walk() {
+    System.out.println("name:" + " " + "is" +" " + "walk");
+    }
+
+     void sit(); {
+        System.out.println("name:" + " " + "is" +" " + "sit");
+    }
+
+        void run(); {
+        System.out.println("name:" + " " + "is" +" " + "run");
+    }
+
+        void speak(); {
+        System.out.println("name:" + " " + "is" +" " + "speak");
+    }
+
+        void learn(); {
+        System.out.println("name:" + " " + "is" +" " + "learn");
+    }
+}
+}
+
+
