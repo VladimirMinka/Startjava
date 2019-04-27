8067a160879f103ba9d827780a762a7f54198d9e
Vladimir.Minka <vladaha@gmail.com>
2019-04-27 23:25:49 +0300

    Добавил решение задачи Wolf-WolfTest

---
+++ Lesson 2/Wolf.java
@@ -0,0 +1,31 @@
+public class Wolf {
+
+    //напишите в нем поля: пол, кличка, вес, возраст, окрас
+    //напишите в нем методы: идти, сидеть, бежать, выть, охотиться
+    String gender;
+    String name;
+    double weight;
+    int age;
+    String color;
+
+    void walk() {
+        System.out.println("Гулять");
+    }
+
+    void sit() {
+        System.out.println("Сидеть");
+    }
+
+    void run() {
+        System.out.println("Бежать");
+    }
+
+    void howl() {
+        System.out.println("Выть");
+    }
+
+    void hunt() {
+        System.out.println("Охотиться");
+    }
+}
+
