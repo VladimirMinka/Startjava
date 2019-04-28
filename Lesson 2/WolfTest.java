eec8ee23f56a9a36a367b036c4360dedfbe33151
Vladimir.Minka <vladaha@gmail.com>
2019-04-28 22:56:37 +0300

    Исправил код в соответствии с замечаниями

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -1,29 +1,30 @@
-class WolfTest {
-
-public static void main(String[] args) {
     //создайте объект типа Wolf
     //присвойте в нем полям экземпляра класса Wolf какие-то значения
     //считайте эти значения из полей и отобразите в консоли
     //вызовите методы объекта
+class WolfTest {
 
-    Wolf wolf = new Wolf();
-    wolf.gender = "man";
-    wolf.name = "Shaitan";
-    wolf.weight = 30.5;
-    wolf.age = 5;
-    wolf.color = "grey";
+public static void main(String[] args) {
 
-    System.out.println(wolf.gender);
-    System.out.println(wolf.name);
-    System.out.println(wolf.weight);
-    System.out.println(wolf.age);
-    System.out.println(wolf.color);
 
-    wolf.walk();
-    wolf.sit();
-    wolf.run();
-    wolf.howl();
-    wolf.hunt();
+        Wolf wolf = new Wolf();
+        wolf.gender = "man";
+        wolf.name = "Shaitan";
+        wolf.weight = 30.5;
+        wolf.age = 5;
+        wolf.color = "grey";
+
+        System.out.println(wolf.gender);
+        System.out.println(wolf.name);
+        System.out.println(wolf.weight);
+        System.out.println(wolf.age);
+        System.out.println(wolf.color);
+
+        wolf.walk();
+        wolf.sit();
+        wolf.run();
+        wolf.howl();
+        wolf.hunt();
     }
 }
 
